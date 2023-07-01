e0b8212024294796ee66a8ed851bd0596b4158d3
DevonWoodman <dwoodman.dw@gmail.com>
2023-07-01 05:56:05 +0200

    Update base_app.py

--- base_app.py
+++ base_app.py
@@ -59,9 +59,9 @@ from sklearn.feature_extraction.text import CountVectorizer
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-nltk.download('omw-1.4')
-nltk.download('wordnet')
-nltk.download('stopwords')
+# nltk.download('omw-1.4')
+# nltk.download('wordnet')
+# nltk.download('stopwords')
 stemmer = SnowballStemmer('english')
 lemmatizer = WordNetLemmatizer()
 tokeniser = TreebankWordTokenizer()
